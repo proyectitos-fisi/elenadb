f2f35c007b2aed9e75f0f976085942c4efd91ac2
Paolo Flores <pflores.fisi22@gmail.com>
2024-06-17 20:22:10 -0500

    add buffer pool manager

--- pkg/buffer/buffer_pool_manager.go
+++ pkg/buffer/buffer_pool_manager.go
@@ -1,1 +1,20 @@
 package buffer
+
+import (
+	storage_disk "fisi/elenadb/pkg/storage/disk"
+	"fisi/elenadb/pkg/storage/page"
+	"sync"
+)
+
+type BufferPoolManager struct {
+	poolSize      uint32
+	pages         page.Page
+	diskScheduler storage_disk.DiskScheduler
+	pageTable     map[uint32]page.Page
+	replacer      LRUKReplacer
+	latch         sync.RWMutex
+}
+
+func (bp *BufferPoolManager) FetchPage() *page.Page {
+	return nil
+}
